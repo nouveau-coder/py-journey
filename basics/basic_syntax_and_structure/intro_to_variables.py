2e42e14e90b92f5a962172eda927b07bfebbe472
Kaarthik <kaarthik.puma@gmail.com>
2025-09-27 21:19:46 +0530

    successfully completed covering the basics of python to the maximum extent i know

---
+++ basics/basic_syntax_and_structure/intro_to_variables.py
@@ -0,0 +1,9 @@
+#now that we got a basic understanding of python syntax, let's learn about variables.
+#unlike some other programming languages, python does not require you to declare a variable before using it.
+#variables are only references to objects in memory, unlike other programming languages where variables are containers for data.
+a = 'hello'
+print(a)  #this line of code will print the value of the variable 'a' to the console.
+b = 5
+print(b) #this line of code will print the value of the variable 'b' to the console.
+a = b 
+print(a)
---
+++ basics/basic_syntax_and_structure/print_statement_intro.py
@@ -0,0 +1,3 @@
+#the start of it all. the very first program most people write when learning a new programming language.
+print('Hello, World!')
+#this line of code prints the text 'Hello, World!' to the console.
